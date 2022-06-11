65c23176b6152ce3638b99a9cf362ce74816138b
DosuSolomon <dosusulaimon.s@gmail.com>
2022-06-11 23:29:48 +0100

    updated config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// const withPlugins = require("next-compose-plugins");
-// const optimizedImages = require("next-optimized-images")
-
-// module.exports = withPlugins([optimizedImages], {target: "serverless"})
 
 const nextConfig = {
   reactStrictMode: true,
